7b1fb3533d5902fe9a18789898c0e1f418a9acfe
tasneem1998 <tasnee.k@homesfy.in>
2021-05-23 11:05:13 +0530

    1.Box componet implemented after 1st commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import {Box,} from "@material-ui/core"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Box width={600} boxShadow={1} component="section" p={3}>
+      <h1>Box component</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi sapiente expedita quo consequuntur consequatur quisquam iste hic optio, voluptas explicabo. Facere aperiam repudiandae ea sapiente neque architecto maiores laudantium? Laboriosam.</p>
+    </Box>
+    </>
   );
 }
 
